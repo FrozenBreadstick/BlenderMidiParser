7e6db18d2c4722c5cb0a5b5792523e13596a229b
Kingpin2557 <lode.snaet@student.howest.be>
2026-02-08 15:07:05 +0100

    feat: Add MIDI parsing and upload functionality
    This commit introduces the core functionality for handling MIDI files.
    It includes:
    - Express.js setup for API endpoints.
    - Integration with `@tonejs/midi` for parsing MIDI data.
    - Multer for handling file uploads, with validation for MIDI file types.
    - API endpoints for uploading MIDI files and retrieving MIDI data.
    - Basic error handling for file operations and upload processing.

--- api/app.js
+++ api/app.js
@@ -1,12 +1,70 @@
+const fs = require("fs");
+const { Midi } = require("@tonejs/midi");
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+const filterType = function (req, file, cb) {
+  const allowedTypes = ["audio/midi", "audio/mid", "audio/x-midi"];
+
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid file type. Only MIDI files are allowed!"), false);
+  }
+};
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
 
-app.post("/upload", (req, res) => {
-  res.send("Uploaded Successfully");
+const upload = multer({
+  storage: storage,
+  fileFilter: filterType,
+});
+
+app.get("/", (req, res) => {
+  try {
+    const [fileName] = fs.readdirSync("./uploads");
+
+    if (!fileName) {
+      return res.status(404).json({ error: "Folder is empty" });
+    }
+
+    const buffer = fs.readFileSync(`./uploads/${fileName}`);
+
+    res.json({
+      filename: fileName,
+      midi: new Midi(buffer),
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Could not read file" });
+  }
+});
+
+app.post("/upload", upload.single("midi"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded." });
+  }
+
+  const midiData = new Midi(fs.readFileSync(req.file.path));
+
+  res.json({
+    message: "File uploaded successfully",
+    midi: midiData,
+  });
+});
+
+app.use((err, req, res, next) => {
+  if (err) {
+    return res.status(400).json({ error: err.message });
+  }
+  next();
 });
 
 const port = process.env.PORT || 3000;
