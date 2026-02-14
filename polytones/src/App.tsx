895bbe08deacc6bc9f318424bc0e3fc1be1cd62e
Kingpin2557 <lode.snaet@student.howest.be>
2026-02-14 17:41:23 +0100

    Remove unnecessary dependencies and update polytones visualization dependencies.

--- polytones/src/App.tsx
+++ polytones/src/App.tsx
@@ -1,25 +1,9 @@
 import "./styles/main.scss";
-import { useState } from "react";
-import { useMidi } from "./hooks/useMidi";
-
-import { db } from "./scripts/db";
-import { useLiveQuery } from "dexie-react-hooks";
-
-import Visualizer from "./components/Visualizer";
 
 function App() {
-  const midi = useLiveQuery(() => db.MidiStorage.get(1));
-
-  const [refreshKey, setRefreshKey] = useState(0);
-  useMidi(refreshKey);
-
-  const handleUploadSuccess = () => setRefreshKey((prev) => prev + 1);
   return (
     <section>
-      <div className="c-input">
-        <button onClick={handleUploadSuccess}>Simulate New File Sync</button>
-      </div>
-      <Visualizer midi={midi?.midi} refreshKey={refreshKey} />
+      <div className="c-input"></div>
     </section>
   );
 }
